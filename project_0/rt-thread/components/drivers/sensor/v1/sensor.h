84e5d059ea2014ebfbbc22961bd70f852552ff74
guozhanxin <guozhanxin@rt-thread.com>
2023-07-26 14:35:24 +0800

    更好的兼容软件包v1

---
+++ project_0/rt-thread/components/drivers/sensor/v1/sensor.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2006-2023, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ */
+
+#include <drivers/sensor.h>
